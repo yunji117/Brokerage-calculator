0b37f276066da14e053c6edd55373a7134fb62c7
yunji <yunw0117@gamil.com>
2026-03-25 17:16:39 +0900

    feat: favicon cash edit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,8 @@ import { siteConfig } from '../lib/siteConfig';
 
 export default function App({ Component, pageProps }: AppProps) {
   const homepageUrl = siteConfig.siteUrl || '';
+  const previewImagePath = '/brokerage-calculator%20Thumbnailimg.png';
+  const faviconPath = '/favicon-v2.png';
 
   return (
     <>
@@ -25,12 +27,12 @@ export default function App({ Component, pageProps }: AppProps) {
         {homepageUrl ? <meta property="og:url" content={homepageUrl} /> : null}
         {/* og:image는 배포 후 추가하세요 */}
         {/* <meta property="og:image" content="https://yourdomain.com/og-image.png" /> */}
-        <meta property="og:image" content={`${homepageUrl}/brokerage-calculator Thumbnailimg.png`} />
+        {homepageUrl ? <meta property="og:image" content={`${homepageUrl}${previewImagePath}`} /> : null}
         <meta property="og:image:alt" content="중개 수수료(복비) 계산기 미리보기 이미지" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
 
-        <meta name="twitter:image" content={`${homepageUrl}/brokerage-calculator Thumbnailimg.png`} />
+        {homepageUrl ? <meta name="twitter:image" content={`${homepageUrl}${previewImagePath}`} /> : null}
 
         
         {/* Twitter 카드 */}
@@ -40,8 +42,9 @@ export default function App({ Component, pageProps }: AppProps) {
         {/* <meta name="twitter:image" content="https://yourdomain.com/og-image.png" /> */}
         
         {/* Favicon */}
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="icon" type="image/png" href={faviconPath} />
+        <link rel="shortcut icon" href={faviconPath} />
+        <link rel="apple-touch-icon" href={faviconPath} />
         {homepageUrl ? <link rel="canonical" href={homepageUrl} /> : null}
         
         {/* 추가 SEO 설정 */}
